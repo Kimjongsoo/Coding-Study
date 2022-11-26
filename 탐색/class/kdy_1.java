88a0e860f11086b433b9aa62bc45b22845841652
Doyeon04 <rlaehdus0411@naver.com>
2022-11-26 14:32:33 +0900

    1번 문제 bfs 방식 추가

--- 탐색/class/kdy_1.java
+++ 탐색/class/kdy_1.java
@@ -1,13 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+class Point{
+    int x, y;
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
 public class Main{
     static int[][] arr;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     static int n, m;
+    static Queue<Point> queue = new LinkedList<>();
 
     public static void dfs(int x, int y){
         for(int i=0; i<4; i++){
@@ -19,6 +29,24 @@ public class Main{
             if(arr[nx][ny]==0){
                 arr[nx][ny] = 1;
                 dfs(nx, ny); 
+            }
+        }
+    }
+
+    public static void bfs(int x, int y){
+        queue.add(new Point(x, y));
+        while(!queue.isEmpty()){
+            Point p = queue.poll();
+            for(int i=0; i<4; i++){
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+
+                if(nx<0 || ny<0 || nx >=n || ny >=m) continue;
+
+                if(arr[nx][ny] == 0){
+                    arr[nx][ny] = 1;
+                    queue.add(new Point(nx, ny));
+                }
             }
         }
     }
@@ -46,7 +74,8 @@ public class Main{
             for(int j=0; j<m; j++){
                 if(arr[i][j] == 0){
                     answer++;
-                    dfs(i, j);
+                    // dfs(i, j);
+                    bfs(i, j);
                 }
             }
         }
